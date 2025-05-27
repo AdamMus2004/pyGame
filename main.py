dc2b8bf73ccf3c15c43015409b75d74e3e79f9c8
szympik <adrian.paluch04@gmail.com>
2025-05-27 17:17:22 +0200

    plansza

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-print("siema")
+import pygame
 import sys
-print(sys.version)
-print("spec od białej broni")
-print("tymek łach to cwel")
-print('rel mocno')
+import os
+
+WIDTH, HEIGHT = 900, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Magia i Miecz")
+pygame.init()
+
+# Wczytaj obraz planszy
+board_image = pygame.image.load(os.path.join("img", "Plansza.png"))
+
+
+# Główna pętla gry
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Wyświetlanie planszy
+    screen.fill((0, 0, 0))  # Czyszczenie ekranu
+    screen.blit(board_image, (0, 0))  # Wyświetlenie obrazu planszy
+    
+    pygame.display.flip()  # Aktualizacja ekranu
+
+pygame.quit()
+sys.exit()
